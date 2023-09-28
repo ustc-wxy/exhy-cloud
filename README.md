abfa48ab81bf753b48f3b9d543646296bec22ada
ustc-wxy <1019495690@qq.com>
2023-09-28 22:37:34 +0800

    Add Manager code skeleton

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ An distributed orchestrator consists of basic components.
 2. Accept tasks to run from a manager.
 3. Provide relevant statistics to manager for the purpose of scheduling tasks.
 4. Keep track of its tasks and their state.
+
+## Manager
+1. Accept requests from users to start and stop tasks.
+2. Schedule tasks onto worker machines.
+3. Keep track of tasks. their states, and the machine on which they run.
--- manager/manager.go
+++ manager/manager.go
@@ -1,1 +1,29 @@
 package manager
+
+import (
+	"container/list"
+	"exhy-cloud/task"
+
+	"github.com/google/uuid"
+)
+
+type Manager struct {
+	Pending       list.List
+	TaskDb        map[string][]task.Task
+	EventDb       map[string][]task.TaskEvent
+	Workers       []string
+	WorkerTaskMap map[string][]uuid.UUID
+	TaskWorkerMap map[uuid.UUID]string
+}
+
+func (m *Manager) SelectWorker() {
+	//toDo
+}
+
+func (m *Manager) UpdateTasks() {
+	//toDo
+}
+
+func (m *Manager) SendWork() {
+	//toDo
+}
