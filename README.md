063cee3e90c5cc02339c8d8f8a78ba1a66d3fbb9
ustc-wxy <1019495690@qq.com>
2023-09-28 21:03:38 +0800

    Add definition of Worker

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # exhy-cloud
 An distributed orchestrator consists of basic components. 
+
+## Worker
+1. Run task as Docker containers.
+2. Accept tasks to run from a manager.
+3. Provide relevant statistics to manager for the purpose of scheduling tasks.
+4. Keep track of its tasks and their state.
--- worker/worker.go
+++ worker/worker.go
@@ -1,1 +1,30 @@
 package worker
+
+import (
+	"container/list"
+	"exhy-cloud/task"
+
+	"github.com/google/uuid"
+)
+
+type Worker struct {
+	Queue     list.List
+	Db        map[uuid.UUID]task.Task
+	TaskCount int
+}
+
+func (w *Worker) CollectStats() {
+	//toDo
+}
+
+func (w *Worker) RunTask() {
+	//toDo
+}
+
+func (w *Worker) StartTask() {
+	//toDo
+}
+
+func (w *Worker) StopTask() {
+	//toDo
+}
