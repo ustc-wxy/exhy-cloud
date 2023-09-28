c5439fa11537b0ebefada80435f94de620b14c47
ustc-wxy <1019495690@qq.com>
2023-09-28 23:43:11 +0800

    Add definition of node

--- node/node.go
+++ node/node.go
@@ -1,1 +1,11 @@
 package node
+
+type Node struct {
+	Name            string
+	Ip              string
+	Memory          int
+	MemoeyAllocated int
+	Disk            int
+	DiskAllocated   int
+	TaskCount       int
+}
