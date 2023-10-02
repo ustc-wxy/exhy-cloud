bd1584073fc5c4d64ab938892705a3debe464032
ustc-wxy <1019495690@qq.com>
2023-10-02 15:29:31 +0800

    Support start or stop a docker container

--- main.go
+++ main.go
@@ -1,1 +1,66 @@
 package main
+
+import (
+	"exhy-cloud/task"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/docker/docker/client"
+)
+
+func main() {
+	fmt.Println("[Main] start...")
+	d, r := createContainer()
+	if r.Error != nil {
+		log.Printf("[Main] Create container failed: %v\n", r.Error)
+		os.Exit(1)
+	}
+	time.Sleep(time.Second * 5)
+	log.Printf("[Main] stopping container %s\n", r.ContainerId)
+	r = stopContainer(d)
+	if r.Error != nil {
+		log.Printf("[Main] Stop container failed: %v\n", r.Error)
+		os.Exit(1)
+	}
+}
+
+func createContainer() (*task.Docker, *task.DockerResult) {
+	c := task.Config{
+		Name:  "test-container-1",
+		Image: "mysql:5.7",
+		Env: []string{
+			"MYSQL_ROOT_PASSWORD=123456",
+		},
+	}
+
+	dc, _ := client.NewClientWithOpts(client.FromEnv)
+	d := task.Docker{
+		Client: dc,
+		Config: c,
+	}
+
+	result := d.Run()
+	if result.Error != nil {
+		fmt.Printf("create container failed: %v\n", result.Error)
+		return nil, nil
+	}
+
+	fmt.Printf(
+		"Container %s is running with config %v\n",
+		result.ContainerId, c)
+	return &d, &result
+}
+
+func stopContainer(d *task.Docker) *task.DockerResult {
+	res := d.Stop()
+	if res.Error != nil {
+		return nil
+	}
+	fmt.Printf(
+		"Container %s has been stopped and removed\n",
+		res.ContainerId,
+	)
+	return &res
+}
--- task/docker.go
+++ task/docker.go
@@ -1,18 +1,28 @@
 package task
 
-import "github.com/docker/docker/client"
+import (
+	"context"
+	"io"
+	"log"
+	"os"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
+)
 
 type Config struct {
-	Name         string
-	AttachStdin  bool
-	AttachStdout bool
-	AttachStderr bool
-	Cmd          []string
-	Image        string
-	Memory       int64
-	Disk         int64
-	Env          []string
-	ResultPolicy string
+	Name          string
+	AttachStdin   bool
+	AttachStdout  bool
+	AttachStderr  bool
+	Cmd           []string
+	Image         string
+	Memory        int64
+	Disk          int64
+	Env           []string
+	RestartPolicy string
 }
 
 type Docker struct {
@@ -26,4 +36,114 @@ type DockerResult struct {
 	Action      string
 	ContainerId string
 	Result      string
+}
+
+// Run() performs the same operations as the `docker run` command
+func (d *Docker) Run() DockerResult {
+	ctx := context.Background()
+
+	// Pull the specified image
+	reader, err := d.Client.ImagePull(ctx, d.Config.Image, types.ImagePullOptions{})
+	if err != nil {
+		log.Printf(
+			"Error pulling image %s: %v\n",
+			d.Config.Image, err,
+		)
+		return DockerResult{Error: err}
+	}
+	io.Copy(os.Stdout, reader)
+
+	// Pre-work for creating container
+	rp := container.RestartPolicy{
+		Name: d.Config.RestartPolicy,
+	}
+
+	r := container.Resources{
+		Memory: d.Config.Memory,
+	}
+
+	containerConfig := container.Config{
+		Image: d.Config.Image,
+		Env:   d.Config.Env,
+	}
+
+	containerHostConfig := container.HostConfig{
+		RestartPolicy:   rp,
+		Resources:       r,
+		PublishAllPorts: true,
+	}
+
+	// Create the container
+	resp, err := d.Client.ContainerCreate(
+		ctx, &containerConfig, &containerHostConfig, nil, nil, d.Config.Name)
+	if err != nil {
+		log.Printf(
+			"Error creating container using image %s : %v\n",
+			d.Config.Image, err)
+		return DockerResult{Error: err}
+	}
+
+	err = d.Client.ContainerStart(
+		ctx, resp.ID, types.ContainerStartOptions{})
+	if err != nil {
+		log.Printf(
+			"Error starting container %s : %v",
+			resp.ID, err)
+		return DockerResult{Error: err}
+	}
+
+	d.ContainerId = resp.ID
+
+	// Show container logs
+	out, err := d.Client.ContainerLogs(
+		ctx, resp.ID,
+		types.ContainerLogsOptions{ShowStdout: true, ShowStderr: true})
+	if err != nil {
+		log.Printf("Error getting logs for container %s : %v\n", resp.ID, err)
+		return DockerResult{Error: err}
+	}
+	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+
+	return DockerResult{
+		ContainerId: resp.ID,
+		Action:      "start",
+		Result:      "success",
+	}
+}
+
+// Stop() performs the same operations as the `docker run` & `docker rm` command
+func (d *Docker) Stop() DockerResult {
+	ctx := context.Background()
+
+	log.Printf(
+		"Attempting to stop container %v",
+		d.ContainerId)
+
+	// Stop container
+	err := d.Client.ContainerStop(ctx, d.ContainerId, container.StopOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	removeOptions := types.ContainerRemoveOptions{
+		RemoveVolumes: true,
+		RemoveLinks:   false,
+		Force:         false,
+	}
+
+	// Remove container
+	err = d.Client.ContainerRemove(
+		ctx,
+		d.ContainerId,
+		removeOptions,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	return DockerResult{
+		Action: "stop",
+		Result: "success",
+		Error:  nil,
+	}
 }
